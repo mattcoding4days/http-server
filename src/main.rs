d20fb8ea53446db98cdec641410fbc6d10d5deec
Matt Williams <mattinzhuhai@outlook.com>
2021-10-07 01:54:10 -0700

    :godmode: Small changes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
 fn main() {
-    println!("Hello, world!");
+    //let server = Server::new("127.0.0.1:8080".to_string());
+
+    let string = String::from("127.0.0.1:8080");
+    let string_slice = &string[10..];
+
+    dbg!(&string);
+    dbg!(string_slice);
+}
+
+struct Server {
+    addr: String,
+}
+
+impl Server {
+    // associated functions are known as static functions in C++
+    fn new(addr: String) -> Self {
+        Server {
+            addr
+        }
+    }
+
+    fn run(self) {
+        println!("Running server on {}", self.addr);
+    }
 }
