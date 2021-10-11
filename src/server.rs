6703ac106b70ed7d761f0d88ad596294f93ff880
Matt Williams <mattinzhuhai@outlook.com>
2021-10-10 23:49:42 -0700

    :godmode: Still trying to figure this language out

--- src/server.rs
+++ src/server.rs
@@ -1,14 +1,48 @@
+use crate::http::Request;
+use std::convert::TryFrom;
+use std::io::Read;
+use std::net::TcpListener;
+
 pub struct Server {
-    addr: String,
+    m_addr: String,
 }
 
 impl Server {
     // associated functions are known as static functions in C++
     pub fn new(addr: String) -> Self {
-        Server { addr }
+        Server { m_addr: addr }
     }
 
-    pub fn run(self) {
-        println!("Running server on {}", self.addr);
+    pub fn run(&self) {
+        println!("Running server on {}", self.m_addr);
+        let listener = TcpListener::bind(&self.m_addr).unwrap();
+        // special syntax for while true
+        loop {
+            match listener.accept() {
+                Ok((mut stream, _)) => {
+                    let mut buffer = [0; 1024];
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            println!(
+                                "Received a request from: {}",
+                                String::from_utf8_lossy(&buffer)
+                            );
+                            match Request::try_from(&buffer[..]) {
+                                Ok(request) => {}
+                                Err(error) => {
+                                    println!("Request failed: {}", error);
+                                }
+                            }
+                        }
+                        Err(error) => {
+                            println!("Failed to read from connection: {}", error);
+                        }
+                    }
+                }
+                Err(error) => {
+                    println!("Failed to establish a connection: {}", error);
+                }
+            }
+        }
     }
 }
