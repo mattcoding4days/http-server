4c65bb366f5878142c361e69c72ee75245632b60
Matt Williams <mattinzhuhai@outlook.com>
2021-10-15 10:59:31 -0700

    :godmode: Updated to reflect new query string module

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -1,7 +1,9 @@
 pub use method::Method;
 pub use parse_error::ParseError;
+pub use query_string::{QueryString, Value as QueryStringValue};
 pub use request::Request;
 
 pub mod method;
-pub mod request;
 pub mod parse_error;
+pub mod query_string;
+pub mod request;
